3c6eba3349536f1da55dd52d443bc5af6a5b8ff8
Indy Lyness <indylyness@Indys-MacBook-Air.local>
2025-04-18 17:39:39 -0500

    Added more features and improved functionality to cli.py.

--- individual/cli.py
+++ individual/cli.py
@@ -5,8 +5,8 @@ Indy Lyness
 
 NAME: cli.py - command line interface exercise
 SYNOPSIS: python3 cli.py event gender 
-DESCRIPTION: Return the first specified number of performances that 
-are better than a specified cutoff for a specified event.
+DESCRIPTION: Return performances filtered by an event and gender, with optional bounds on the time/mark.
+The number of performances displayed and whethere there are multiple for each individual athlete can also be specified.
 """
 
 import argparse
@@ -19,9 +19,11 @@ def get_parsed_arguments():
     parser = argparse.ArgumentParser(description='Filter performances based on event and time/mark.')
     parser.add_argument('event', help='Event that you would like to filter for. Examples: \"400 Meters\", \"Shot Put\", etc.')
     parser.add_argument('gender', help='Gender to filter for. Options are \'m\' for men and \'f\' for women.')
-    parser.add_argument('--cutoff', '-c', help='Optional cutoff to filter performances by. For running events, this should be a time (mm:ss.xx), and for field events this should be a mark (12.37m or 4\' 2.5\").')
+    parser.add_argument('--upper', '-p', help='Upper bound to filter performances by. For running events, this should be a time (mm:ss.xx), and for field events this should be a mark (12.37m or 4\' 2.5\"). For the decathlon, this should be a point value (e.g. 5000).')
+    parser.add_argument('--lower', '-l', help='Lower bound to filter performances by. See --upper description for formatting notes.')
     parser.add_argument('--conv', action='store_true', help='Whether to recognize the cutoff mark as feet\' inches\" format (default is meters - if you put feet/inches format and do not specify this flag you will get an error).')
-    parser.add_argument('--number', '-n', default=10, help='Number of performances to display (displays all performances if there are less than the given number after filtering).')
+    parser.add_argument('--number', '-n', default=10, help='Maximum number of performances to display (displays all performances if there are less than the given number after filtering).')
+    parser.add_argument('--unique', '-u', action='store_true', help='Whether to display just the best performance for each athlete.')
     parsed_arguments = parser.parse_args()
     return parsed_arguments
 
@@ -48,10 +50,13 @@ def convert_mark(mark):
         return
     if "m" in mark:
         return float(mark[:-1])
-    sep = mark.split('\'')
-    feet = int(sep[0])
-    inches = float(sep[1][:-1])
-    return 12*feet + inches
+    elif '\'' in mark:
+        sep = mark.split('\'')
+        feet = int(sep[0])
+        inches = float(sep[1][:-1])
+        return 12*feet + inches
+    else:
+        return int(mark)
 
 def display_performances(performances, format):
     """
@@ -61,7 +66,8 @@ def display_performances(performances, format):
         print("No performances matching filter.")
         return
     type = performances[0]["Result"]
-    header = "Athlete".ljust(20) + "| Year".ljust(8) + "| School".ljust(20) + "| Event".ljust(18) + f"| {type}".ljust(12) + "| Date".ljust(15) + "| Meet".ljust(20)
+    header = "Athlete".ljust(20) + "| Year".ljust(8) + "| School".ljust(22) + "| Event".ljust(20) + f"| {type}".ljust(12) + "| Date".ljust(15) + "| Meet".ljust(20)
+    print("-"*150)
     print(header)
     print("-"*150)
     for row in performances:
@@ -69,16 +75,19 @@ def display_performances(performances, format):
             mins = row["Time"] // 60
             secs = row["Time"] % 60
             if mins > 0:
-                result_string = f"{mins:04.0f}:{secs:04.2f}"
+                result_string = f"{mins:02.0f}:{secs:05.2f}"
             else:
-                result_string = f"{secs:04.2f}"
-        elif format == False:
-            result_string = f"{row["Mark"]}m"
+                result_string = f"{secs:05.2f}"
+        elif type == "Mark":
+            if format == False:
+                result_string = f"{row["Mark"]}m"
+            else:
+                feet = row["Conv"] // 12
+                inches = row["Conv"] % 12
+                result_string = f"{feet:.0f}\'{inches:04.2f}\""
         else:
-            feet = row["Conv"] // 12
-            inches = row["Conv"] % 12
-            result_string = f"{feet:.0f}\'{inches:04.2f}\""
-        display = f"{row['Athlete']}".ljust(20) + f"| {row['Year']}".ljust(8) + f"| {row['School']}".ljust(20) + f"| {row['Event']}".ljust(18) + f"| {result_string}".ljust(12) + f"| {row['Meet Date']}".ljust(15) + f"| {row['Meet']}".ljust(20)
+            result_string = row["Points"]
+        display = f"{row['Athlete']}".ljust(20) + f"| {row['Year']}".ljust(8) + f"| {row['School']}".ljust(22) + f"| {row['Event']}".ljust(20) + f"| {result_string}".ljust(12) + f"| {row['Meet Date']}".ljust(15) + f"| {row['Meet']}".ljust(20)
         print(display)
         print("-"*150)
 
@@ -115,31 +124,51 @@ def get_performances(arguments):
         row["Mark"] = convert_mark(row["Mark"])
         row["Conv"] = convert_mark(row["Conv"])
         row["Time"] = convert_time(row["Time"])
+        row["Points"] = int(row["Points"]) if row["Points"] != "" else None
 
-    # Filter by gender
+    # Filter by gender and event
     if arguments.gender == "m":
         data = list(filter(lambda x: x["Category"]=='m', data))
     else:
         data = list(filter(lambda x: x["Category"]=='f', data))
 
-    # Filter by event and optional cutoff using standard filter function and relevant dictionary key
-    if event_key[arguments.event] == "Time":
-        performances = list(filter(lambda x: x["Event"]==arguments.event, data))
-        performances.sort(key=lambda x:x["Time"])
-        performances = performances[:int(arguments.number)]
-        if arguments.cutoff:
-            performances = list(filter(lambda x: x["Time"] < convert_time(arguments.cutoff), performances))
-    else:
-        performances = list(filter(lambda x: x["Event"]==arguments.event, data))
-        performances.sort(key=lambda x:x["Mark"], reverse=True)
-        performances = performances[:int(arguments.number)]
-        if arguments.cutoff:
-            if arguments.conv:
-                performances = list(filter(lambda x: x["Conv"] > convert_mark(arguments.cutoff), performances))
-            else:
-                performances = list(filter(lambda x: x["Mark"] > convert_mark(arguments.cutoff), performances))
+    performances = list(filter(lambda x: x["Event"]==arguments.event, data))
 
-    return performances
+    # Filter for any bounds provided for time/mark
+    if event_key[arguments.event] == "Time":
+        performances.sort(key=lambda x:x[event_key[arguments.event]])
+        if arguments.lower:
+            performances = list(filter(lambda x: x["Time"] > convert_time(arguments.lower), performances))
+        if arguments.upper:
+            performances = list(filter(lambda x: x["Time"] < convert_time(arguments.upper), performances))
+    else:
+        performances.sort(key=lambda x:x[event_key[arguments.event]], reverse=True)
+        if arguments.lower:
+            if arguments.conv:
+                performances = list(filter(lambda x: x["Conv"] > convert_mark(arguments.lower), performances))
+            else:
+                performances = list(filter(lambda x: x[event_key[arguments.event]] > convert_mark(arguments.lower), performances))
+        if arguments.upper:
+            if arguments.conv:
+                performances = list(filter(lambda x: x["Conv"] < convert_mark(arguments.upper), performances))
+            else:
+                performances = list(filter(lambda x: x[event_key[arguments.upper]] < convert_mark(arguments.cutoff), performances))
+
+    # Filter down to best performance for each athlete if specified
+    if arguments.unique:
+        athletes = set()
+        final_performances = []
+        for row in performances:
+            if row["Athlete"] not in athletes:
+                athletes.add(row["Athlete"])
+                final_performances.append(row)
+    else:
+        final_performances = performances
+
+    final_performances = final_performances[:int(arguments.number)]
+    
+
+    return final_performances
 
 def main():
     arguments = get_parsed_arguments()
