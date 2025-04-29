c7fa1c0d7673a7ecf478ce68625ab8c89db73fad
Indy Lyness <indy.lyness@gmail.com>
2025-04-29 13:33:06 -0500

    Set athletes table to contain school

--- data/database-schema.sql
+++ data/database-schema.sql
@@ -2,18 +2,9 @@ CREATE TABLE athletes (
     id integer NOT NULL,
     last_name text,
     first_name text,
+    school text,
     gender text
 );
-
-CREATE TABLE schools (
-    id integer NOT NULL,
-    school_name text
-);
-
-CREATE TABLE athletes_schools (
-    athlete_id integer,
-    school_id integer
-)
 
 CREATE TABLE events (
     id integer NOT NULL,
