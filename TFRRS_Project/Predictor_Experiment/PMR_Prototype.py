8c97d0685442ce229f1f212c7efb0e2773ad27bd
decsdan <scheiderd@carleton.edu>
2025-04-29 17:44:58 -0500

    looking

---
+++ TFRRS_Project/Predictor_Experiment/PMR_Prototype.py
@@ -0,0 +1,7 @@
+#This is meant to prototype using Multivariate Polynomial Regression to predict "time fitness"
+#E.G using multiple variables (weather, races run before)
+# to predict a polynomial fit (nonlinear)
+
+
+#currently isolating to 1 athlete, 1 event, and not including weather or prior races
+
