fb1af573cd9d79bc4789adee581ad8d2fa259ebe
Indy Lyness <indy.lyness@gmail.com>
2025-04-29 13:03:02 -0500

    Fix to api.py path

--- individual/api.py
+++ individual/api.py
@@ -45,7 +45,7 @@ def convert_mark(mark):
     else:
         return int(mark)
 
-with open('data/MIAC_data.csv') as csvfile:
+with open('../data/MIAC_data.csv') as csvfile:
     data = []
     reader = csv.reader(csvfile,  delimiter=',')
     header = reader.__next__()
